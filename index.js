93b319b4c551096f268197f2bb4af8044a4f8378
Aman2414 <amanw.zenzzen@gmail.com>
2022-10-12 19:44:30 +0530

    Server Added

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const app = express();
 const routes = require("./controllers/route");
+const http = require("http");
 const port = process.env.PORT || 1337;
 
 app.use("/", routes);
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`);
-  console.log("Server Started");
-});
+
+var server = http.createServer(app).listen(port);
