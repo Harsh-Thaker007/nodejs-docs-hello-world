1227f3d4f218958e19dcee1f81e00e759fe89756
Aman2414 <amanw.zenzzen@gmail.com>
2022-10-12 19:49:48 +0530

    Ok

--- controllers/route.js
+++ controllers/route.js
@@ -3,9 +3,10 @@ const path = require("path");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.sendFile(
-    path.join(__dirname.replace("controllers", "/website/index.html"))
-  );
+  //   res.sendFile(
+  //     path.join(__dirname.replace("controllers", "/website/index.html"))
+  //   );
+  res.send("helo World");
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const routes = require("./controllers/route");
-const http = require("http");
+const http = require('http')
 const port = process.env.PORT || 1337;
 
 app.use("/", routes);
