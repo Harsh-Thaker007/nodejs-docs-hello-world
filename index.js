91100b3e5b257863189b3611b8166731f02fd823
Aman2414 <amanw.zenzzen@gmail.com>
2022-10-12 20:51:53 +0530

    Removed Router full

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const routes = require("./controllers/route");
 const port = process.env.PORT || 1337;
 // app.use(express.static("website"));
 
@@ -8,9 +7,7 @@ app.get("/", (req, res) => {
   //   res.sendFile(
   //     path.join(__dirname.replace("controllers", "/website/index.html"))
   //   );
-  res.send(
-    "helo World all files are installed and will be displayed shortlyyy"
-  );
+  res.send("Hello World");
 });
 
 app.listen(port, () => {
