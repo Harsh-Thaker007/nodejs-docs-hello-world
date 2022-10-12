8b7e7f60f46d5213c9b31a88ffbf7419dd8bf54e
Aman2414 <amanw.zenzzen@gmail.com>
2022-10-12 19:52:04 +0530

    Removed Server

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
-const routes = require("./controllers/route");
-const http = require('http')
+const routes = require("./controllers/route")
 const port = process.env.PORT || 1337;
-
 app.use("/", routes);
 
-var server = http.createServer(app).listen(port);
+app.listen(port, () => {
+  console.log(`http://localhost:${port}/`);
+  console.log("Server Started");
+});
+
