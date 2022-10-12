9a0b1e9888ec58307ff6eff7ed9ce14ed9bf9fdd
Aman2414 <amanw.zenzzen@gmail.com>
2022-10-12 21:11:39 +0530

    Removed INdex Entrypoint

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const port = process.env.PORT || 1337;
 app.use(express.static("website"));
 
 app.get("/", (req, res) => {
-  res.send("Server Is Running");
-});
-
-app.get("/index", (req, res) => {
+  // res.send("Server Is Running");
   res.sendFile(path.join(__dirname + "/website/index.html"));
 });
+
+// app.get("/index", (req, res) => {
+// });
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}/`);
