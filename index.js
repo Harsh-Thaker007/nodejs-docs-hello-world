1a0c4b68b07ffb7c30ae37873760a723cabf2608
Aman2414 <amanw.zenzzen@gmail.com>
2022-10-12 20:14:48 +0530

    Removed Router

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require("express");
 const app = express();
 const routes = require("./controllers/route");
 const port = process.env.PORT || 1337;
-app.use("/", routes);
 // app.use(express.static("website"));
+
+app.get("/", (req, res) => {
+  //   res.sendFile(
+  //     path.join(__dirname.replace("controllers", "/website/index.html"))
+  //   );
+  res.send(
+    "helo World all files are installed and will be displayed shortlyyy"
+  );
+});
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}/`);
