34662622c87398358d198748edfc37ad540bda49
Aman2414 <amanw.zenzzen@gmail.com>
2022-10-12 19:56:58 +0530

    Removed all html files

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-const routes = require("./controllers/route")
+const routes = require("./controllers/route");
 const port = process.env.PORT || 1337;
 app.use("/", routes);
+app.use("/", express.static(__dirname + "/"));
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}/`);
   console.log("Server Started");
 });
-
