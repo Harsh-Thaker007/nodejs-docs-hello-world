71cc48e6fc053ed183050ba64ca876795e77000e
Aman2414 <amanw.zenzzen@gmail.com>
2022-10-12 19:59:54 +0530

    All solved If

--- controllers/route.js
+++ controllers/route.js
@@ -6,7 +6,7 @@ router.get("/", (req, res) => {
   //   res.sendFile(
   //     path.join(__dirname.replace("controllers", "/website/index.html"))
   //   );
-  res.send("helo World");
+  res.send("helo World all files are installed and will be displayed shortlyyy");
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const routes = require("./controllers/route");
 const port = process.env.PORT || 1337;
 app.use("/", routes);
-app.use("/", express.static(__dirname + "/"));
+app.use(express.static("website"));
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}/`);
